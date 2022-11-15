ad94b5ae1f63127fe77908682eba402840b86466
muralidhulipalla <mkdhuli@yahoo.com>
2022-11-14 20:56:31 -0600

    Initial Add

---
+++ src/main/java/com/example/springhellogradle/SpringHelloGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhellogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelloGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springhellogradle/SpringHelloGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhellogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
